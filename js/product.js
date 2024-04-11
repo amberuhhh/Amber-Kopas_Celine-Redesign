8127701f82cffc57ddf5abe3b19897425509f975
amberuhhh <amberkopas12@gmail.com>
2024-04-11 18:25:46 -0400

    bruh

--- js/product.js
+++ js/product.js
@@ -32,46 +32,60 @@ var client = contentful.createClient({
   accessToken: 'z05I6zWTRyoI5LJIlwwrhUEVSBHgEKoWpeljnS75yrc',
 });
 
-var placeProduct = document.getElementById('product');
-  // gets all the entries as a json
+
+
+var placeProduct = document.getElementById('image');
+// gets all the entries as a json
   client.getEntry(id).then(function (entry) {
     console.log(entry);
       // if statement checks that this field exists  
       var imageDiv = document.createElement ('aside');
-      var products = document.createElement ('div');
 
           if (entry.fields.productName) {
-
             var productMainImage = document.createElement('img');
             productMainImage.src = entry.fields.productMainImage.fields.file.url;
             imageDiv.appendChild(productMainImage);
 
-            var productName = document.createElement('h4');
-            productName.innerHTML = entry.fields.productName;
-            products.appendChild(productName);
-
-            var productBasicInfo = document.createElement('p');
-            productBasicInfo.innerHTML = entry.fields.productBasicInfo;
-            products.appendChild(productBasicInfo);
-
-            var productPrice = document.createElement('h6');
-            productPrice.innerHTML = entry.fields.productPrice;
-            products.appendChild(productPrice);
-
-            var productCare = document.createElement('p');
-            productCare.innerHTML = entry.fields.productCare;
-            products.appendChild(productCare);
-
-            var productDetails = document.createElement('p');
-            productDetails.innerHTML = entry.fields.productDetails;
-            products.appendChild(productDetails);
-
             console.log(entry.fields.productName);
-        }  
+          }
         placeProduct.appendChild(imageDiv);
-        placeProduct.appendChild(products);
       });
 
+
+
+  var placeProductPara = document.getElementById('textdiv');
+  var placeProducthead = document.getElementById('textdiv');
+    client.getEntry(id).then(function (entry) {
+    console.log(entry);
+      var paraDiv = document.createElement ('aside');
+      var headings = document.createElement ('div');
+
+      if (entry.fields.productName) {
+        var productName = document.createElement('h4');
+        productName.innerHTML = entry.fields.productName;
+        headings.appendChild(productName);
+
+        var productBasicInfo = document.createElement('p');
+        productBasicInfo.innerHTML = entry.fields.productBasicInfo;
+        headings.appendChild(productBasicInfo);
+
+        var productPrice = document.createElement('h6');
+        productPrice.innerHTML = entry.fields.productPrice;
+        headings.appendChild(productPrice);
+        
+        var productCare = document.createElement('p');
+        productCare.innerHTML = entry.fields.productCare;
+        paraDiv.appendChild(productCare);
+
+        var productDetails = document.createElement('p');
+        productDetails.innerHTML = entry.fields.productDetails;
+        paraDiv.appendChild(productDetails);
+
+        console.log(entry.fields.productName);
+      }
+    placeProducthead.appendChild(headings);
+    placeProductPara.appendChild(paraDiv);
+  });
 
 
 
@@ -118,7 +132,7 @@ client.getEntries({content_type: 'celineRedesign', limit:3,}).then(function (ent
       console.log(entry.fields.productName);
       } 
       placeForContent.appendChild(dailyPicks);
-  });
+    });
 });
 
 
